6406585f193f71a3639099b414db4264ab8036d5
Eva Danti Rahmanita <evadantir@yahoo.com>
2017-01-11 17:25:02 +0700

    Update drawTransformation and twoPoint

--- Mini Paint/Mini Paint/DrawTransformation.cs
+++ Mini Paint/Mini Paint/DrawTransformation.cs
@@ -11,27 +11,32 @@ namespace Mini_Paint
     {
         mathMethod mm = new mathMethod();
 
-        public void Dilatasi(Point p0,Point p1, Graphics g)
+        public twoPoint Dilatasi(Point p0,Point p1, int kX, int kY)
         {
-
+            twoPoint pTrans = new twoPoint();
+            pTrans.x0 = p0.X * kX;
+            pTrans.y0 = p0.Y * kY;
+            pTrans.x1 = p1.X * kX;
+            pTrans.y1 = p1.Y * kY;
+            return pTrans;
         }
         
-        public Point Translasi(Point p0, Point p1, int dX, int dY)
+        public twoPoint Translasi(Point p0, Point p1, int dX, int dY)
         {
-            Point pStart = p0,pEnd = p1;
-            pStart.X = pStart.X + dX;
-            pStart.Y = pStart.Y + dY;
-            pEnd.X = pEnd.X + dX;
-            pEnd.Y = pEnd.Y + dY;
-            return pEnd;
+            twoPoint pTrans = new twoPoint();
+            pTrans.x0 = p0.X + dX;
+            pTrans.y0 = p0.Y + dY;
+            pTrans.x1 = p1.X + dX;
+            pTrans.y1= p1.Y + dY;
+            return pTrans;
         }
 
-        public void Rotasi(Point p0, Point p1, Graphics g)
+        public twoPoint Rotasi(Point p0, Point p1, Graphics g)
         {
-
+            
         }
 
-        public void Refleksi(Point p0, Point p1, Graphics g)
+        public twoPoint Refleksi(Point p0, Point p1, Graphics g)
         {
 
         }
--- Mini Paint/Mini Paint/twoPoint.cs
+++ Mini Paint/Mini Paint/twoPoint.cs
@@ -9,19 +9,32 @@ namespace Mini_Paint
 {
     class twoPoint
     {
-        private Point _p0;
-        private Point _p1;
-        
-        public Point p0
+        private int _x0;
+        private int _x1;
+        private int _y0;
+        private int _y1;
+
+        public int x0
         {
-            set { _p0 = value; }
-            get { return _p0; }
+            set { _x0 = value; }
+            get { return _x0; }
         }
 
-        public Point p1
+        public int x1
         {
-            set { _p1 = value; }
-            get { return _p1; }
+            set { _x1 = value; }
+            get { return _x1; }
+        }
+        public int y0
+        {
+            set { _y0 = value; }
+            get { return _y0; }
+        }
+
+        public int y1
+        {
+            set { _y1 = value; }
+            get { return _y1; }
         }
 
         public twoPoint()
@@ -29,10 +42,12 @@ namespace Mini_Paint
 
         }
 
-        public twoPoint(Point p0, Point p1)
+        public twoPoint(int x0, int x1, int y0, int y1)
         {
-            _p0 = p0;
-            _p1 = p1;
+            _x0 = x0;
+            _x1 = x1;
+            _y0 = y0;
+            _y1 = y1;
         }
     }
 }
